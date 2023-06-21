e230d270f57df7ef95113ad495b80c0d4bc9bff4
David Michel <david.michel2809@gmail.com>
2023-06-21 15:02:03 +0200

    refacto: rework FormLayout structure and style

--- src/components/FormLayout/FormLayout.tsx
+++ src/components/FormLayout/FormLayout.tsx
@@ -2,13 +2,14 @@ import styles from "./formLayout.module.css"
 
 interface Props {
   children: JSX.Element | JSX.Element[]
+  onSubmit: (event: React.FormEvent) => void
 }
 
-const FormLayout = ({ children }: Props) => {
+const FormLayout = ({ children, onSubmit }: Props) => {
   return (
-    <div className={styles.layout}>
+    <form onSubmit={onSubmit} className={styles.form}>
       {children}
-    </div>
+    </form>
   )
 }
 export default FormLayout
