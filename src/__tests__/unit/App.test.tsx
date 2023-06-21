deded37e3c9a30c91606bb9087e051c030fda31d
David Michel <david.michel2809@gmail.com>
2023-06-21 07:41:35 +0200

    config: add testing environment

---
+++ src/__tests__/unit/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "../../App";
+
+describe("init", () => {
+  it("Should be true", () => {
+    expect(true).toBeTruthy()
+  })
+})
