7d2ee0e2f265df0aa5affed77e87b385f87b55ba
David Michel <david.michel2809@gmail.com>
2023-06-26 13:47:13 +0200

    feat: add className props to Avatar

--- src/Components/Avatar/Avatar.tsx
+++ src/Components/Avatar/Avatar.tsx
@@ -3,13 +3,14 @@ import avatarPlaceholder from "./avatar-placeholder.jpg"
 
 interface Props {
   size: "medium" | "small"
+  className?: string
 }
 
-function Avatar({ size }: Props) {
-  const className = `${styles.avatar} ${styles[size]}`
+function Avatar({ size, className }: Props) {
+  const classNames = `${styles.avatar} ${styles[size]} ${className ? className : ""}`
 
   return (
-    <div className={className}>
+    <div className={classNames}>
       <img src={avatarPlaceholder} alt="Avatar de l'utilisateur" />
     </div>
   )
