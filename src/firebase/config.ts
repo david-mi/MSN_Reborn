a346d9d553763495831761c01bf43c7776859c6e
David Michel <david.michel2809@gmail.com>
2023-06-29 11:42:35 +0200

    config: add firebase storage service and modify rules

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp, } from "firebase/app"
 import { getAuth, connectAuthEmulator } from "firebase/auth"
+import { getStorage, connectStorageEmulator } from "firebase/storage"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,9 +15,11 @@ const firebaseConfig = {
 const fireBaseApp = initializeApp(firebaseConfig);
 
 export const firebase = {
-  auth: getAuth(fireBaseApp)
+  auth: getAuth(fireBaseApp),
+  storage: getStorage(fireBaseApp)
 }
 
 if (import.meta.env.MODE === "development") {
   connectAuthEmulator(firebase.auth, "http://127.0.0.1:9099")
+  connectStorageEmulator(firebase.storage, "127.0.0.1", 9199)
 }
