f6918a042490c728e28f27a5d3c3a99ddb36fa40
David Michel <david.michel2809@gmail.com>
2023-07-02 16:28:40 +0200

    feat: add more general setup to tests

--- tests.setup.ts
+++ tests.setup.ts
@@ -8,5 +8,7 @@ expect.extend(matchers);
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
+  vi.clearAllMocks()
   vi.resetAllMocks()
+  vi.restoreAllMocks()
 });
