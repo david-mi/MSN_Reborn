40a9cd9584b8443111f7b60ed43bffc1e9d73e3c
David Michel <david.michel2809@gmail.com>
2023-07-05 10:54:28 +0200

    config: add firestore config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 import { initializeApp, } from "firebase/app"
 import { getAuth, connectAuthEmulator } from "firebase/auth"
 import { getStorage, connectStorageEmulator } from "firebase/storage"
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,10 +17,12 @@ const fireBaseApp = initializeApp(firebaseConfig);
 
 export const firebase = {
   auth: getAuth(fireBaseApp),
-  storage: getStorage(fireBaseApp)
+  storage: getStorage(fireBaseApp),
+  firestore: getFirestore(fireBaseApp)
 }
 
 if (import.meta.env.MODE === "development" || process.env.NODE_ENV === "test") {
   connectAuthEmulator(firebase.auth, "http://127.0.0.1:9099")
   connectStorageEmulator(firebase.storage, "127.0.0.1", 9199)
+  connectFirestoreEmulator(firebase.firestore, "127.0.0.1", 8080)
 }
