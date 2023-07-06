958c05b8ef45041ab404e97e6ef95c722955d5a2
David Michel <david.michel2809@gmail.com>
2023-07-06 14:45:33 +0200

    feat: add periodic check on EmailVerification to see if user is verified

--- src/Components/Register/EmailVerification/EmailVerification.tsx
+++ src/Components/Register/EmailVerification/EmailVerification.tsx
@@ -2,20 +2,41 @@ import styles from "./emailVerification.module.css";
 import Button from "@/Components/Shared/Button/Button";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
 import { sendVerificationEmail } from "@/redux/slices/register/register";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Loader from "@/Components/Shared/Loader/Loader";
 import Infos from "./Infos/Infos";
+import { UserService } from "@/Services";
+import { setStep } from "@/redux/slices/register/register";
 
 function EmailVerification() {
   const dispatch = useAppDispatch()
   const { submitStatus, submitError } = useAppSelector(state => state.register)
+  const verifyTimeout = useRef<NodeJS.Timer>()
 
   function handleClick() {
     dispatch(sendVerificationEmail())
   }
 
+  async function handleSendVerificationEmail() {
+    verifyTimeout.current = setTimeout(async () => {
+      clearTimeout(verifyTimeout.current)
+      const isUserVerified = await UserService.checkIfVerified()
+
+      if (isUserVerified) {
+        dispatch(setStep("VERIFIED"))
+      } else {
+        handleSendVerificationEmail()
+      }
+    }, 2000)
+  }
+
   useEffect(() => {
-    dispatch(sendVerificationEmail())
+    (async function () {
+      await dispatch(sendVerificationEmail())
+      handleSendVerificationEmail()
+    })()
+
+    handleSendVerificationEmail()
   }, [])
 
   return (
