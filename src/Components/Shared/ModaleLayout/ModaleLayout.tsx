17ac5dff030a9bece0dc84bec072a344e45d9a3a
David Michel <david.michel2809@gmail.com>
2023-07-24 11:54:17 +0200

    feat: add optional close functionality on ModaleLayout

--- src/Components/Shared/ModaleLayout/ModaleLayout.tsx
+++ src/Components/Shared/ModaleLayout/ModaleLayout.tsx
@@ -1,17 +1,31 @@
+import { useState } from "react"
 import msnLogo from "/msn-logo.png"
 import styles from "./modaleLayout.module.css"
+import { CloseButton } from ".."
 
 export interface Props {
   title: string
   children: JSX.Element | JSX.Element[]
+  closable?: boolean
 }
 
-function ModaleLayout({ title, children }: Props) {
+function ModaleLayout({ title, children, closable = false }: Props) {
+  const [closedModale, setClosedModale] = useState(false)
+
+  if (closedModale) {
+    return null
+  }
+
+  function closeModale() {
+    setClosedModale(true)
+  }
+
   return (
     <div className={styles.layout}>
       <header className={styles.header}>
         <img className={styles.logo} src={msnLogo} />
         <h1 className={styles.title}>{title}</h1>
+        {closable && <CloseButton type="button" onClick={closeModale} />}
       </header>
       {children}
     </div >
