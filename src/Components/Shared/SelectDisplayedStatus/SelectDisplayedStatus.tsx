78945f2e61dffe27824a21776eb71170390023f4
David Michel <david.michel2809@gmail.com>
2023-07-25 08:35:26 +0200

    refacto: add possibility to open DisplayedStatusesList by default on props

--- src/Components/Shared/SelectDisplayedStatus/SelectDisplayedStatus.tsx
+++ src/Components/Shared/SelectDisplayedStatus/SelectDisplayedStatus.tsx
@@ -7,11 +7,16 @@ import styles from "./selectShowStatus.module.css";
 interface Props {
   setStatus: (status: DisplayedStatus) => void
   defaultStatus?: string
+  defaultListOpen?: boolean
 }
 
-function SelectDisplayedStatus({ setStatus, defaultStatus = statusesObject.online.sentence }: Props) {
+function SelectDisplayedStatus({
+  setStatus,
+  defaultStatus = statusesObject.online.sentence,
+  defaultListOpen = false
+}: Props) {
   const [statusSentence, setStatusSentence] = useState(defaultStatus)
-  const [showStatusesList, setShowStatusesList] = useState(false)
+  const [showStatusesList, setShowStatusesList] = useState(defaultListOpen)
 
   return (
     <div className={styles.wrapper}>
