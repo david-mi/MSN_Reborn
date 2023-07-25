d532216aa211d3e5e92133e25c1e14530b298acd
David Michel <david.michel2809@gmail.com>
2023-07-25 08:03:03 +0200

    refacto/style remove styles from CloseIcon, add disabled style to CloseButton

--- src/Components/Shared/Icons/CloseIcon/CloseIcon.tsx
+++ src/Components/Shared/Icons/CloseIcon/CloseIcon.tsx
@@ -1,9 +1,7 @@
 
-import styles from "./closeIcon.module.css"
-
 function CloseIcon() {
   return (
-    <svg className={styles.closeIcon} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64">
       <path d="M62 10.571L53.429 2L32 23.429L10.571 2L2 10.571L23.429 32L2 53.429L10.571 62L32 40.571L53.429 62L62 53.429L40.571 32z">
       </path>
     </svg>
