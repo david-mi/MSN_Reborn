04c49121e7f73053ef6a83abca334930fc923c31
David Michel <david.michel2809@gmail.com>
2023-07-26 10:54:10 +0200

    feat: implement button to add contact in Heading

--- src/Components/Home/Menu/Contacts/Heading/Heading.tsx
+++ src/Components/Home/Menu/Contacts/Heading/Heading.tsx
@@ -1,8 +1,14 @@
+import { ButtonWithImage } from "@/Components/Shared";
+import addContactIcon from "/icons/add-contact.png"
 import styles from "./heading.module.css";
 
 function Heading() {
   return (
     <div className={styles.heading}>
+      <ButtonWithImage
+        src={addContactIcon}
+        className={styles.addContactButton}
+      />
     </div>
   );
 }
