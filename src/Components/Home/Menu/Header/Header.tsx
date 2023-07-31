e0c9da3f749eb063c8aafe239266b6afea2c6910
David Michel <david.michel2809@gmail.com>
2023-07-31 09:42:55 +0200

    feat: add DisconnectButton and implement it on Menu Header

--- src/Components/Home/Menu/Header/Header.tsx
+++ src/Components/Home/Menu/Header/Header.tsx
@@ -1,11 +1,13 @@
-import styles from "./header.module.css";
+import DisconnectButton from "./DisconnectButton/DisconnectButton";
 import windowsLogo from "./windows-logo.png"
+import styles from "./header.module.css";
 
 function Header() {
   return (
     <header className={styles.header}>
       <img src={windowsLogo} alt="Logo windows" />
       <h1 className={styles.title}>MSN <b>Reborn</b></h1>
+      <DisconnectButton />
     </header>
   );
 }
--- src/Components/Home/Menu/Menu.tsx
+++ src/Components/Home/Menu/Menu.tsx
@@ -1,22 +1,14 @@
-import { useAppDispatch } from "@/redux/hooks";
-import { disconnect } from "@/redux/slices/user/user";
 import { Header, Profile, Contacts } from ".";
 import { GradientLayout } from "@/Components/Shared";
 import styles from "./menu.module.css"
 
 function Menu() {
-  const dispatch = useAppDispatch()
-
-  function handleClick() {
-    dispatch(disconnect())
-  }
-
   return (
     <GradientLayout className={styles.menu} data-testid="menu">
       <Header />
       <Profile />
       <Contacts />
-      <button onClick={handleClick}>Se déconnecter</button>
+
     </GradientLayout>
   );
 }
