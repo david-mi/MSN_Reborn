4b74e6c0af675e6fd940df0bc2b6234d7008ac9b
David Michel <david.michel2809@gmail.com>
2023-08-17 13:38:06 +0200

    fix: call setcurrentDisplayedRoom only if the targeted room is different than the current one

--- src/Components/Home/Menu/Contacts/ContactsList/ContactsList.tsx
+++ src/Components/Home/Menu/Contacts/ContactsList/ContactsList.tsx
@@ -2,17 +2,24 @@ import { ImageLoadWrapper, Avatar } from "@/Components/Shared";
 import useContact from "@/hooks/useContact";
 import styles from "./contactsList.module.css"
 import { statusesObject } from "@/Components/Shared/SelectDisplayedStatus/statusesData";
-import { useAppDispatch } from "@/redux/hooks";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setcurrentDisplayedRoom } from "@/redux/slices/room/room";
 import useRoom from "@/hooks/useRoom";
 
 function ContactsList() {
+  const currentRoomId = useAppSelector(({ room }) => room.currentRoomId)
   const dispatch = useAppDispatch()
   const { contacts, contactsError } = useContact()
   useRoom()
 
   if (contactsError) {
     return <small>Une erreur est survenue lors de la récupération des contacts</small>
+  }
+
+  function handleContactClick(roomId: string) {
+    if (roomId !== currentRoomId) {
+      dispatch(setcurrentDisplayedRoom(roomId))
+    }
   }
 
   return (
@@ -22,7 +29,7 @@ function ContactsList() {
           <li
             className={styles.contactsList}
             key={id}
-            onClick={() => dispatch(setcurrentDisplayedRoom(roomId))}
+            onClick={() => handleContactClick(roomId)}
           >
             <div className={styles.avatarContainer}>
               <Avatar src={avatarSrc} size="small" />
