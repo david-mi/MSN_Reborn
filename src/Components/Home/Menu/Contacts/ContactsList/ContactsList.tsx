5ce1128a8ce06e59551c07478215bf49c72949be
David Michel <david.michel2809@gmail.com>
2023-08-18 13:59:04 +0200

    refacto: add DisplayedContact component

--- src/Components/Home/Menu/Contacts/ContactsList/ContactsList.tsx
+++ src/Components/Home/Menu/Contacts/ContactsList/ContactsList.tsx
@@ -1,14 +1,9 @@
-import { ImageLoadWrapper, Avatar } from "@/Components/Shared";
 import useContact from "@/hooks/useContact";
 import styles from "./contactsList.module.css"
-import { statusesObject } from "@/Components/Shared/SelectDisplayedStatus/statusesData";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { setcurrentDisplayedRoom } from "@/redux/slices/room/room";
 import useRoom from "@/hooks/useRoom";
+import Contact from "./DisplayedContact/DisplayedContact";
 
 function ContactsList() {
-  const currentRoomId = useAppSelector(({ room }) => room.currentRoomId)
-  const dispatch = useAppDispatch()
   const { contacts, contactsError } = useContact()
   useRoom()
 
@@ -16,33 +11,9 @@ function ContactsList() {
     return <small>Une erreur est survenue lors de la récupération des contacts</small>
   }
 
-  function handleContactClick(roomId: string) {
-    if (roomId !== currentRoomId) {
-      dispatch(setcurrentDisplayedRoom(roomId))
-    }
-  }
-
   return (
     <ul className={styles.container}>
-      {contacts.map(({ username, displayedStatus, avatarSrc, personalMessage, roomId, id }) => {
-        return (
-          <li
-            className={styles.contactsList}
-            key={id}
-            onClick={() => handleContactClick(roomId)}
-          >
-            <div className={styles.avatarContainer}>
-              <Avatar src={avatarSrc} size="small" />
-              <ImageLoadWrapper
-                imageProps={{ src: statusesObject[displayedStatus].icon, alt: "icône du status de l'utilisateur" }}
-                loaderOptions={{ size: "16px", thickness: "2px" }}
-              />
-            </div>
-            <span>{username}</span>
-            <small> {personalMessage}</small>
-          </li>
-        )
-      })}
+      {contacts.map((contact) => <Contact key={contact.id} contact={contact} />)}
     </ul>
   );
 }
---
+++ src/Components/Home/Menu/Contacts/ContactsList/DisplayedContact/DisplayedContact.tsx
@@ -0,0 +1,42 @@
+import { Contact } from "@/redux/slices/contact/types";
+import { statusesObject } from "@/Components/Shared/SelectDisplayedStatus/statusesData";
+import { ImageLoadWrapper, Avatar } from "@/Components/Shared";
+import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { setcurrentDisplayedRoom } from "@/redux/slices/room/room";
+import styles from "./displayedContact.module.css";
+
+interface Props {
+  contact: Contact
+}
+
+function DisplayedContact({ contact }: Props) {
+  const { username, displayedStatus, avatarSrc, personalMessage, roomId, id } = contact
+  const dispatch = useAppDispatch()
+  const currentRoomId = useAppSelector(({ room }) => room.currentRoomId)
+
+  function handleContactClick(roomId: string) {
+    if (roomId !== currentRoomId) {
+      dispatch(setcurrentDisplayedRoom(roomId))
+    }
+  }
+
+  return (
+    <li
+      className={styles.displayedContact}
+      key={id}
+      onClick={() => handleContactClick(roomId)}
+    >
+      <div className={styles.avatarContainer}>
+        <Avatar src={avatarSrc} size="small" />
+        <ImageLoadWrapper
+          imageProps={{ src: statusesObject[displayedStatus].icon, alt: "icône du status de l'utilisateur" }}
+          loaderOptions={{ size: "16px", thickness: "2px" }}
+        />
+      </div>
+      <span>{username}</span>
+      <small> {personalMessage}</small>
+    </li>
+  );
+}
+
+export default DisplayedContact;
