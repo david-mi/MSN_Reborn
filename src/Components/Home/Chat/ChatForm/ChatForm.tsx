a389694223ceaa3cc46ee542f2f65cfafb85c882
David Michel <david.michel2809@gmail.com>
2023-09-30 11:37:39 +0200

    refacto: add submit button for chat messages in mobile version only, prevent handleKeyDown for mobiles

--- src/Components/Home/Chat/ChatForm/ChatForm.tsx
+++ src/Components/Home/Chat/ChatForm/ChatForm.tsx
@@ -7,6 +7,7 @@ import { MessageValidation } from "@/utils/Validation";
 import styles from "./chatForm.module.css";
 import { RoomUsers } from "@/redux/slices/room/types";
 import ChatFormOptions from "./ChatFormOptions/ChatFormOptions";
+import { ButtonWithSvg } from "@/Components/Shared";
 
 interface Props {
   roomId: string
@@ -38,6 +39,8 @@ function ChatForm({ roomId, users, shouldScrollToBottomRef }: Props) {
   }
 
   function handleKeyDown(event: KeyboardEvent) {
+    if (matchMedia("(max-width: 750px)").matches) return
+
     if (event.key === "Enter" && event.shiftKey === false) {
       handleSubmit(onSubmit)()
       shouldScrollToBottomRef.current = true
@@ -57,6 +60,11 @@ function ChatForm({ roomId, users, shouldScrollToBottomRef }: Props) {
         onKeyDown={handleKeyDown}
         disabled={sendMessageRequest.status === "PENDING"}
       />
+      <ButtonWithSvg className={styles.submitButton}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+          <path fill="currentColor" d="m3.4 20.4l17.45-7.48a1 1 0 0 0 0-1.84L3.4 3.6a.993.993 0 0 0-1.39.91L2 9.12c0 .5.37.93.87.99L17 12L2.87 13.88c-.5.07-.87.5-.87 1l.01 4.61c0 .71.73 1.2 1.39.91z"></path>
+        </svg>
+      </ButtonWithSvg>
       <small>{errors.content?.message}</small>
     </form >
   );
